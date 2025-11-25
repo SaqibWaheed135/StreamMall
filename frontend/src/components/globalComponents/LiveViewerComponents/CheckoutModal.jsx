f4b15f6612467293a9c6d87c363b2aed1d2a5e9e
Saqib135 <xaqibwaheed333@gmail.com>
2025-11-25 16:26:11 +0500

    fix: usestate issue and bucket issue from live stream

--- frontend/src/components/globalComponents/LiveViewerComponents/CheckoutModal.jsx
+++ frontend/src/components/globalComponents/LiveViewerComponents/CheckoutModal.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { API_BASE_URL } from "../../../config/api";
 import {  X } from 'lucide-react';
 
--- frontend/src/components/globalComponents/LiveViewerComponents/StreamEndedModal.jsx
+++ frontend/src/components/globalComponents/LiveViewerComponents/StreamEndedModal.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { AlertCircle, Clock, Eye, HeartIcon } from 'lucide-react';
 
 const StreamEndedModal = ({ streamData, onNavigate }) => {
